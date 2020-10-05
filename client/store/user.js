da1da114e31e059834d28d68cf09f30a662d6785
Sarah Park <sarahyaepark@gmail.com>
2020-10-04 23:23:03 -0700

    Feature: Question 2

--- client/store/user.js
+++ client/store/user.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
-import history from '../history'
 
 /**
  * ACTION TYPES
  */
-const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
+const GET_USERS = 'GET_USERS'
 
 /**
  * INITIAL STATE
@@ -15,56 +13,30 @@ const defaultUser = {}
 /**
  * ACTION CREATORS
  */
-const getUser = user => ({type: GET_USER, user})
-const removeUser = () => ({type: REMOVE_USER})
+const gotUsers = data => ({type: GET_USERS, data})
 
 /**
  * THUNK CREATORS
  */
-export const me = () => async dispatch => {
-  try {
-    const res = await axios.get('/auth/me')
-    dispatch(getUser(res.data || defaultUser))
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-export const auth = (email, password, method) => async dispatch => {
-  let res
-  try {
-    res = await axios.post(`/auth/${method}`, {email, password})
-  } catch (authError) {
-    return dispatch(getUser({error: authError}))
-  }
-
-  try {
-    dispatch(getUser(res.data))
-    history.push('/home')
-  } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
-  }
-}
-
-export const logout = () => async dispatch => {
-  try {
-    await axios.post('/auth/logout')
-    dispatch(removeUser())
-    history.push('/login')
-  } catch (err) {
-    console.error(err)
+export const fetchUsers = () => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/users')
+      dispatch(gotUsers(data))
+      console.log(data)
+    } catch (err) {
+      console.log('ERROR fetching users', err)
+    }
   }
 }
 
 /**
  * REDUCER
  */
-export default function(state = defaultUser, action) {
+export default function(state = [], action) {
   switch (action.type) {
-    case GET_USER:
-      return action.user
-    case REMOVE_USER:
-      return defaultUser
+    case GET_USERS:
+      return action.data
     default:
       return state
   }
